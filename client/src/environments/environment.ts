7cf2c2afd1ebaf93cc886e0baa64e6236b28cb6a
HassanKhan8264 <HF610061@GMAIL.COM>
2024-07-19 13:42:41 +0500

    change app name

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
-  appName: "Portalize",
+  appName: "Dayone",
   production: false,
   HOST: "http://127.0.0.1:5001/",
   getUrl() {
